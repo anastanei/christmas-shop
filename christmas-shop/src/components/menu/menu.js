50d25ee6aa3ff2c3f43b4d6373a89fed573cd067
Anastasiia Nikonova <anastaney@gmail.com>
2024-11-18 21:19:06 +0100

    fix: remove console bugs

--- christmas-shop/src/components/menu/menu.js
+++ christmas-shop/src/components/menu/menu.js
@@ -1,9 +1,7 @@
 export default class Menu {
-  constructor(burgerSelector, navSelector, linkSelector) {
+  constructor(burgerSelector, navSelector) {
     this.nav = document.querySelector(navSelector);
     this.button = document.querySelector(burgerSelector);
-    console.log(linkSelector);
-    // this.links = this.menu.querySelectorAll(linkSelector);
     this.button.addEventListener("click", () => this.toggleMenu());
   }
 
