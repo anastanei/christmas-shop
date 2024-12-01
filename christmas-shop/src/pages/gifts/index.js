2d33c217651f38f6d7e353a0988766c567b21979
Anastasiia Nikonova <anastaney@gmail.com>
2024-12-01 11:55:10 +0100

    feat: add scroll button

--- christmas-shop/src/pages/gifts/index.js
+++ christmas-shop/src/pages/gifts/index.js
@@ -1,5 +1,7 @@
 import createLayout from "../../assets/js/create-layout";
 import initTabs from "../../components/tabs/tabs";
+import initScrollButton from "../../components/scroll-button/scroll-button";
 
 createLayout();
 initTabs();
+initScrollButton();
